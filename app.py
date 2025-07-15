48cc21e96a7d8ac1cf3d97850e4c3ee05820c1ac
Enoch Owoade <enochowoade@gmail.com>
2025-07-14 20:01:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -333,6 +333,8 @@ def api_score():
 
 db.init_app(app)
 migrate = Migrate(app, db)
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True, host="localhost", port=5050)
